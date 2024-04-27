d965be9eb467533f471ddbc3be912d54babc011f
Md Foisal Islam <islammdfoisal54@gmail.com>
2024-04-27 09:31:40 +0600

    in hero section category and slider added

---
+++ src/data/categoryData.js
@@ -0,0 +1,70 @@
+const categories = [
+  {
+    id: 1,
+    name: "Women's & Girls' Fashion",
+    path: '/women-girls',
+    category: 'women',
+  },
+  {
+    id: 2,
+    name: ' Health & Beauty',
+    path: '/health-beauty',
+    category: 'health',
+  },
+  {
+    id: 3,
+    name: 'Watches, Bags, Jewellery',
+    path: '/watch-bags-jewellery',
+    category: 'watch bag jewellery',
+  },
+  {
+    id: 4,
+    name: "Men's & Boys' Fashion",
+    path: '/men-boys',
+    category: 'men',
+  },
+  {
+    id: 5,
+    name: 'Mother & Baby',
+    path: '/mother-baby',
+    category: 'mother baby',
+  },
+  {
+    id: 6,
+    name: 'Electronics Device',
+    path: '/electronics',
+    category: 'electronics',
+  },
+  {
+    id: 7,
+    name: 'TV & Home Appliances',
+    path: '/home-appliances',
+    category: 'home appliances',
+  },
+  {
+    id: 8,
+    name: 'Groceries',
+    path: '/groceries',
+    category: 'groceries',
+  },
+  {
+    id: 9,
+    name: 'Home & Lifestyle',
+    path: '/life-style',
+    category: 'lifestyle',
+  },
+  {
+    id: 10,
+    name: 'Sports & Outdoorse',
+    path: '/sports',
+    category: 'sports',
+  },
+  {
+    id: 11,
+    name: 'Automotive & Motorbike',
+    path: '/motorbike',
+    category: 'motorbike',
+  },
+];
+
+export default categories;
--- src/pages/Home/HeroSection/HeroSection.jsx
+++ src/pages/Home/HeroSection/HeroSection.jsx
@@ -1,4 +1,10 @@
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import {
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+  Autoplay,
+} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -7,12 +13,20 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './HeroSection.css';
 import sliderdata from '../../../data/sliderdata';
+import { Link } from 'react-router-dom';
+import categories from '../../../data/categoryData';
 
 const HeroSection = () => {
   return (
     <main className="flex my-5 justify-between gap-5 items-center">
-      <div className="hidden border border-slate-200 p-5  rounded-xl lg:block w-[300px] shadow-sm">
-        categories section
+      <div className="hidden border border-slate-200 p-5  rounded-xl lg:block w-[300px] shadow-md">
+        <ul className="font-semibold space-y-[13px] text-gray-500">
+          {categories.map(category => (
+            <li className="hover:text-[#f85606] text-xs" key={category.id}>
+              <Link to={category.path}> {category.name} </Link>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="flex-1 w-full lg:w-[calc(1024px-300px)]">
         <Swiper
@@ -21,7 +35,8 @@ const HeroSection = () => {
           pagination={true}
           mousewheel={true}
           keyboard={true}
-          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          autoplay={true}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
           className="mySwiper w-full"
         >
           {sliderdata.map(item => (
